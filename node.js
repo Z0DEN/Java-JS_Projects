ce3d6ea8c305c325bca7311301d6382b2314b20b
Z0DEN <maksarestov2000@gmail.com>
2023-03-25 02:50:47 +0700

    +parser+modules

--- node.js
+++ node.js
@@ -1,1 +1,27 @@
-console.log("Hello Node")
+const axios = require('axios'); // Подключение модуля axios для скачивания страницы
+const fs = require('fs'); // Подключение встроенного в Node.js модуля fs для работы с файловой системой
+const jsdom = require("jsdom"); // Подключение модуля jsdom для работы с DOM-деревом (1)
+const { JSDOM } = jsdom; // Подключение модуля jsdom для работы с DOM-деревом (2)
+
+const link = 'https://www.goodfon.ru/catalog/animals/'; // Типовая ссылка на страницу со статьями (без номера в конце)
+
+function paginator() {
+    // console.log('Запрос статей по ссылке: ' + link); // Уведомление о получившейся ссылке
+    // Запрос к странице сайта
+    axios.get(link)
+    .then(response => {
+        var currentPage = response.data; // Запись полученного результата
+        const dom = new JSDOM(currentPage); // Инициализация библиотеки jsdom для разбора полученных HTML-данных, как в браузере
+        var linksLength = dom.window.document.getElementsByClassName('wallpapers__item__wall').length
+        console.log(linksLength)
+        for (i = 0; i < linksLength; i++) {
+                var relLink = dom.window.document.getElementsByClassName('wallpapers__item__wall')[i].getElementsByTagName('img')[0].outerHTML;
+                console.log(relLink + "\n")
+                fs.appendFileSync('C:/Users/user_10/GitHub/cloudblesk.site/www/Wallpapers/images.html', relLink + "\n", (err) => {
+                    if (err) throw err;
+                });
+        }
+        console.log('Парсинг завершён.'); // Уведомление об окончании работы парсера
+    });
+};
+paginator(); // Запуск перехода по страницам и сбора статей
