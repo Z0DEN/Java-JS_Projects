3915960b29ce14fa67c73e9efb18193944ffeabb
Z0DEN <maksarestov2000@gmail.com>
2023-04-15 02:00:07 +0700

    add-user

---
+++ views/appp.js
@@ -0,0 +1,17 @@
+const http = require("http");
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+// async function init() {
+//   console.log("Остановка сервера через 5 секунд");
+//   await delay(5000);
+//   server.close(() => console.log('Server stopped.'));
+// }
+
+var server = http.createServer(function(request,response){
+     
+  response.end("Hello NodeJS!");
+   
+});
+server.listen(3000, "127.0.0.1",function(){
+  console.log("Сервер начал прослушивание запросов на порту 3000");
+});
+// init()
